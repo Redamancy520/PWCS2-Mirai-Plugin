7ac2aabb440ac069459f6b7cd93541d4db9f01c8
herry <79433820+nongbaobao@users.noreply.github.com>
2025-02-04 22:14:16 +0800

    Add Config System

--- src/main/java/cn/imsakura/PWCS.java
+++ src/main/java/cn/imsakura/PWCS.java
@@ -1,26 +1,11 @@
 package cn.imsakura;
 
-import cn.imsakura.apis.DetailStats;
-import cn.imsakura.apis.SearchUser;
-import cn.imsakura.jsonData.search.PwcsResponse;
-import cn.imsakura.jsonData.search.PwcsUser;
-import com.google.gson.Gson;
 import net.mamoe.mirai.Bot;
-import net.mamoe.mirai.console.MiraiConsole;
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
-import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.GlobalEventChannel;
 import net.mamoe.mirai.event.events.BotOnlineEvent;
-import net.mamoe.mirai.event.events.FriendMessageEvent;
-import net.mamoe.mirai.event.events.GroupMessageEvent;
-import net.mamoe.mirai.message.data.Image;
-import net.mamoe.mirai.message.data.MessageChain;
-import net.mamoe.mirai.utils.ExternalResource;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Objects;
+import io.herry.Config;
 
 public final class PWCS extends JavaPlugin {
     public static final PWCS INSTANCE = new PWCS();
@@ -28,12 +13,12 @@ public final class PWCS extends JavaPlugin {
 
 
     public static boolean hasToken(){
-        return Config.INSTANCE.token.length() > 6;
+        return Config.INSTANCE.getToken().length() > 6;
     }
 
     public static Bot bot;
     public static boolean hasMySteamId(){
-        return Config.INSTANCE.mySteamId.length() > 6;
+        return Config.INSTANCE.getMySteamId().length() > 6;
     }
 
     private PWCS() {
@@ -45,8 +30,11 @@ public final class PWCS extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
         getLogger().info("喵喵！[PerfectWorld-CS2-Plugin] 开始加载了捏~~~");
+
+        // init config
+        reloadPluginConfig(Config.INSTANCE);
+
         GlobalEventChannel.INSTANCE.subscribeAlways(BotOnlineEvent.class,botOnlineEvent -> {
             bot = botOnlineEvent.getBot();
             bot.getEventChannel().registerListenerHost(new PrivateMessageListener());
